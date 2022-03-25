ef53552a5c41e5ef4a174a072c70f4a4ca489a21
mochammad-fachorrozi <mochammadfachorrozi98@gmail.com>
2022-03-25 10:20:27 +0700

    menganti gambar di project dan menambah file script

---
+++ script.js
@@ -0,0 +1,51 @@
+// AOS
+const galleryImage = document.querySelectorAll('.gallery-img');
+
+    galleryImage.forEach((img, i) => {
+        img.dataset.aos = 'fade-down';
+        img.dataset.aosDelay = i * 100;
+        img.dataset.aosDuration = 1000;
+    })
+
+AOS.init({
+duration: 2000, 
+once: true,
+});
+
+
+// gsap
+gsap.registerPlugin(TextPlugin);
+    gsap.to('.lead', { duration: 2, delay: 1.5, text: 'Web Developer | Content Creator'});
+    gsap.from('.jumbotron img', {duration: 1, rotateY: 360, opacity: 0});
+    gsap.from('.navbar', { duration: 1.5, y: '-100%', opacity: 0, ease: 'bounce'});
+    gsap.from('.display-4', { duration: 1.5, x: -50, opacity: 0,delay: 0.5, ease: 'back'});
+
+
+// form kirim
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwJH9vv9c2UL2bkkZgNrBsk83mm8wYysvNAYr6R2pFByMPlYF2JJ9js47JeiwEqWoSAjg/exec';
+
+const form = document.forms['mf-contact-form'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+      
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //   ketika tombol submit diklik
+    // tampilkan tombol loading, hilangkan tombol kirim
+    btnLoading.classList.toggle('d-none');
+    btnKirim.classList.toggle('d-none');
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        // tampilkan tombol kirim, hilangkan tombol loading
+        btnLoading.classList.toggle('d-none');
+        btnKirim.classList.toggle('d-none');
+        // tampilkan myAlert
+        myAlert.classList.toggle('d-none');
+        // reset formnya
+        form.reset();
+        console.log('Success!', response);
+        })
+            .catch(error => console.error('Error!', error.message))
+})
