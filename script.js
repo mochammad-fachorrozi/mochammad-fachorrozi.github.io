8d899a5557c357d3e141c7f07e2699d4d577db7d
mochammad-fachorrozi <mochammadfachorrozi98@gmail.com>
2024-09-03 18:25:58 +0700

    update my portfolio

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ once: true,
 
 // gsap
 gsap.registerPlugin(TextPlugin);
-    gsap.to('.lead', { duration: 2, delay: 1.5, text: 'Web Developer | Content Creator'});
+    gsap.to('.lead', { duration: 2, delay: 1.5, text: 'Web Developer '});
     gsap.from('.jumbotron img', {duration: 1, rotateY: 360, opacity: 0});
     gsap.from('.navbar', { duration: 1.5, y: '-100%', opacity: 0, ease: 'bounce'});
     gsap.from('.display-4', { duration: 1.5, x: -50, opacity: 0,delay: 0.5, ease: 'back'});
